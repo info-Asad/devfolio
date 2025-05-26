51a6fee2acce174c68d670c411e95ff0c1cec370
Asad <141823279+info-Asad@users.noreply.github.com>
2025-05-26 23:29:33 +0500

    Initial commit - take some code change

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
+// next.config.js
 const nextConfig = {
-  output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: { unoptimized: true },
+  eslint: { ignoreDuringBuilds: true },
+  images: { unoptimized: true }, // optional, based on usage
 };
 
 module.exports = nextConfig;
